db55d5543755e189f6c4137dd9ec5d38d54d297f
Sim Chan <raindow03@gmail.com>
2025-07-06 02:52:38 +0400

    Добавил хендлеры для получуние и создание продуктов

---
+++ internal/handlers/products/getProducts.go
@@ -0,0 +1,30 @@
+package products
+
+import (
+	"encoding/json"
+	"internship/internal/database"
+	"internship/internal/models"
+	"net/http"
+)
+
+func GetProducts(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+
+	if r.Method != http.MethodGet {
+		w.Header().Set("Allow", "GET")
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	dbpool := database.NewProductDB(database.ConnectDatabase())
+
+	product, err := dbpool.GetProducts()
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(models.ErrorItem{Message: err.Error(), Code: http.StatusBadRequest})
+		return
+	}
+
+	json.NewEncoder(w).Encode(product)
+}
