e9fd6c8ba2736ba5aef92b75b10d62af78437b6b
Sim Chan <raindow03@gmail.com>
2025-07-06 02:18:17 +0400

    Добавлен handler heath

---
+++ internal/handlers/heath.go
@@ -0,0 +1,9 @@
+package handlers
+
+import "net/http"
+
+func Heath(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+
+	w.WriteHeader(http.StatusOK)
+}
