0c037b050a228e576aea65ca31460710753219c7
ChanKachan <raindow03@gmail.com>
2025-07-12 02:07:37 +0400

    fix(inventory): added NOT NULL for quantity_of_product

--- migrations/000004_create_inventory_table.up.sql
+++ migrations/000004_create_inventory_table.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS public.inventory (
     warehouse_id uuid NOT NULL REFERENCES public.warehouses(id),
     product_id uuid NOT NULL REFERENCES public.products(id),
-    quantity_of_product integer DEFAULT 0 CHECK (quantity_of_product >= 0),
+    quantity_of_product integer NOT NULL DEFAULT 0 CHECK (quantity_of_product >= 0),
     price integer NOT NULL CHECK (price >= 0),
     percentage_discount_from_price integer DEFAULT 0 CHECK (percentage_discount_from_price BETWEEN 0 AND 100),
     PRIMARY KEY (warehouse_id, product_id)
